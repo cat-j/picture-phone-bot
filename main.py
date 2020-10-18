de87cd302a70f220addf9a541dd91db2a8447b9d
cat <catalinajuarros@gmail.com>
2020-10-18 14:25:50 -0400

    Refactor text replies

--- main.py
+++ main.py
@@ -3,25 +3,41 @@ from telegram.ext import (
     Updater
 )
 
-class Bot:
+class BotTexts:
 
     def __init__(self):
-        self.token = self.get_token()
+        self.start = "dutch apple ass"
+        self.newgame_group = "dutch apple ass"
+        self.newgame_other = "you're not in a group"
+
+
+class BotRunner:
+
+    def __init__(self):
+        self.token = self._get_token()
         self.updater = Updater(token=self.token, use_context=True)
         self.dispatcher = self.updater.dispatcher
+        self.texts = BotTexts()
 
-    def get_token(self):
-        with open("./token", 'r') as f:
-            return f.read()
+    # PUBLIC
 
     def start(self, update, context):
-        context.bot.send_message(chat_id=update.effective_chat.id, text="dutch apple ass")
+        self._reply_text(update, context, self.texts.start)
 
     def newgame(self, update, context):
         if update.message.chat.type == 'group':
-            context.bot.send_message(chat_id=update.effective_chat.id, text="dutch apple ass")
+            group = update.message.chat
+            print(group)
+            print(type(group))
+            self._reply_text(update, context, self.texts.newgame_group)
         else:
-            context.bot.send_message(chat_id=update.effective_chat.id, text="you're not in a group")
+            self._reply_text(update, context, self.texts.newgame_other)
+
+    def run(self):
+        self._register_commands()
+        self.updater.start_polling()
+
+    # PRIVATE
 
     def _register_commands(self):
         handlers = [
@@ -32,9 +48,13 @@ class Bot:
         for handler in handlers:
             self.dispatcher.add_handler(handler)
 
-    def run(self):
-        self._register_commands()
-        self.updater.start_polling()
+    def _reply_text(self, update, context, text_to_send):
+        context.bot.send_message(chat_id=update.effective_chat.id, text=text_to_send)
 
-raul = Bot()
+    def _get_token(self):
+        with open("./token", 'r') as f:
+            return f.read()
+
+
+raul = BotRunner()
 raul.run()
