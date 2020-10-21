d0b634adcb5cf55340c3ff4cfab8027ce8d74762
cat <catalinajuarros@gmail.com>
2020-10-20 21:08:32 -0400

    Refactor game tests

--- test_1.py
+++ test_1.py
@@ -6,67 +6,66 @@ from picture_phone_game import (
 )
 
 
-def test_0():
+def test_game_can_start_with_at_least_four_players():
     players = [
         "Jimmy McGill",
         "Kim Wexler",
         "Howard Hamlin",
         "Chuck McGill"
     ]
-    game = PicturePhoneGame()
-
-    for player in players:
-        game.join_player(player)
+    game = game_with_players(players)
 
     start = game.start()
 
     assert start
 
 
-def test_1():
+def test_game_cannot_start_with_less_than_four_players():
     players = [
         "Mike Ehrmantraut",
         "Stacey Ehrmantraut",
         "Kaylee Ehrmantraut"
     ]
-    game = PicturePhoneGame()
-
-    for player in players:
-        game.join_player(player)
+    game = game_with_players(players)
 
     with pytest.raises(PicturePhoneGameError):
         game.start()
 
 
-def test_2():
+def test_player_cannot_join_game_twice():
     players = [
         "Jimmy McGill",
         "Kim Wexler",
         "Howard Hamlin",
         "Chuck McGill"
     ]
-    game = PicturePhoneGame()
-
-    for player in players:
-        game.join_player(player)
+    game = game_with_players(players)
 
     with pytest.raises(PicturePhoneGameError):
         game.join_player(players[0])
 
 
-def test_3():
+def test_player_cannot_join_game_already_in_course():
     players = [
         "Jimmy McGill",
         "Kim Wexler",
         "Howard Hamlin",
         "Chuck McGill"
     ]
-    game = PicturePhoneGame()
-
-    for player in players:
-        game.join_player(player)
+    game = game_with_players(players)
 
     game.start()
 
     with pytest.raises(PicturePhoneGameError):
         game.join_player("Rick Schweikart")
+
+
+### HELPERS ###
+
+def game_with_players(players):
+    game = PicturePhoneGame()
+
+    for player in players:
+        game.join_player(player)
+    
+    return game
